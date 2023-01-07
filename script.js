4987e743823f5bc424a7b9d2ea7cd7bc4f6a2f30
WOLFATHASHIK222002 <120623366+WOLFATHASHIK222002@users.noreply.github.com>
2023-01-07 05:22:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,47 @@ function show(){
     var small=document.getElementById("small");
     var medium=document.getElementById("medium");
     var large=document.getElementById("large");
+    var your=document.getElementById("name").value;
     // var myarray=[small,medium,large];
-    var pepper=[pyes,pno]
-    var chees=[cyes,cno]
+    // var pepper=[pyes,pno]
+    // var chees=[cyes,cno]
     var bill=0;
     if(small.checked){
-        bill+=80;
+        bill+=60;
         document.getElementById("s").innerHTML="your pizza size small  :"+bill;
     }
     if(medium.checked){
-        bill+=120;
+        bill+=110;
         document.getElementById("s").innerHTML="your pizza size medium  :"+bill;
     }
+  
     if(large.checked){
-        bill+=160;
+        bill+=140;
         document.getElementById("s").innerHTML="your pizza size large  :"+bill;
     }
+    if(small.checked&&medium.checked){
+        document.getElementById("s").innerHTML="your pizza size small,medium  :"+bill;
+    }
+    if(small.checked&&large.checked){
+        document.getElementById("s").innerHTML="your pizza size small,large :"+bill;
+    }
+    if(medium.checked&&large.checked){
+        document.getElementById("s").innerHTML="your pizza size medium,large  :"+bill;
+    }
+    document.getElementById("s").innerHTML="your pizza size small,medium,large  :"+bill;
+ 
+    
     if(pyes.checked){
-        bill+=10;
+        bill+=5;
         document.getElementById("p").innerHTML="you have add pepper amount:'10'";
     } 
    if(cyes.checked){
-    bill+=20;
+    bill+=10;
     document.getElementById("c").innerHTML="your have add chess amount:'20'";
    }
 
 
    document.getElementById("result").innerHTML="total bill amount : "+bill;
-
+document.getElementById("myname").innerHTML=your;
  
 }
